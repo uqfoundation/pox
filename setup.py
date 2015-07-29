307117f8716b2e0a605c28c97d529e9b008fba9d
mmckerns <mmckerns@8bfda07e-5b16-0410-ab1d-fd04ec2748df>
2015-07-29 00:39:11 +0000

    tag: release pox-0.2.2
    git-svn-id: svn+ssh://svn.mystic.cacr.caltech.edu/pathos/pox@801 8bfda07e-5b16-0410-ab1d-fd04ec2748df

--- setup.py
+++ setup.py
@@ -9,9 +9,9 @@ from __future__ import with_statement
 import os
 
 # set version numbers
-stable_version = '0.2.1'
+stable_version = '0.2.2'
 target_version = '0.2.2'
-is_release = False
+is_release = stable_version == target_version
 
 # check if easy_install is available
 try:
