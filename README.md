498d4b452f909189921fbae5d498a2968882a537
mmckerns <mmckerns@8bfda07e-5b16-0410-ab1d-fd04ec2748df>
2016-07-16 23:36:42 +0000

    fixing README formatting, author info; fix: citation string
    git-svn-id: svn+ssh://svn.mystic.cacr.caltech.edu/pathos/pox@884 8bfda07e-5b16-0410-ab1d-fd04ec2748df

--- README.md
+++ README.md
@@ -4,17 +4,17 @@ utilities for filesystem exploration and automated builds
 
 About Pox
 ---------
-Pox provides a collection of utilities for navigating and manipulating
+`pox` provides a collection of utilities for navigating and manipulating
 filesystems. This module is designed to facilitate some of the low level
 operating system interactions that are useful when exploring a filesystem
 on a remote host, where queries such as "what is the root of the filesystem?",
 "what is the user's name?", and "what login shell is preferred?" become
 essential in allowing a remote user to function as if they were logged in
-locally. While pox is in the same vein of both the `os` and `shutil`
+locally. While `pox` is in the same vein of both the `os` and `shutil`
 builtin modules, the majority of its functionality is unique and compliments
 these two modules.
 
-Pox provides python equivalents of several unix shell commands such as
+`pox` provides python equivalents of several unix shell commands such as
 "which" and "find". These commands allow automated discovery of what has
 been installed on an operating system, and where the essential tools are
 located. This capability is useful not only for exploring remote hosts,
@@ -25,13 +25,13 @@ Examples of which are: finding the location of an installed python package,
 determining if and where the source code resides on the filesystem, and
 determining what version the installed package is.
 
-Pox also provides utilities to enable the abstraction of commands sent
+`pox` also provides utilities to enable the abstraction of commands sent
 to a remote filesystem.  In conjunction with a registry of environment
-variables and installed utilites, pox enables the user to interact with
+variables and installed utilites, `pox` enables the user to interact with
 a remote filesystem as if they were logged in locally. 
 
-Pox is part of pathos, a python framework for heterogeneous computing.
-Pox is in active development, so any user feedback, bug reports, comments,
+`pox` is part of `pathos`, a python framework for heterogeneous computing.
+`pox` is in active development, so any user feedback, bug reports, comments,
 or suggestions are highly appreciated.  A list of known issues is maintained
 at http://trac.mystic.cacr.caltech.edu/project/pathos/query, with a public
 ticket list at https://github.com/uqfoundation/pox/issues.
@@ -39,13 +39,13 @@ ticket list at https://github.com/uqfoundation/pox/issues.
 
 Major Features
 --------------
-Pox provides utilities for discovering the user's environment::
+`pox` provides utilities for discovering the user's environment::
 
 * return the user's name, current shell, and path to user's home directory
 * strip duplicate entries from the user's $PATH
 * lookup and expand environment variables from ${VAR} to 'value'
 
-Pox also provides utilities for filesystem exploration and manipulation::
+`pox` also provides utilities for filesystem exploration and manipulation::
 
 * discover the path to a file, exectuable, directory, or symbolic link 
 * discover the path to an installed package
@@ -55,7 +55,7 @@ Pox also provides utilities for filesystem exploration and manipulation::
 
 Current Release
 ---------------
-The latest stable release of pox is available from::
+The latest released version of `pox` is available from::
     http://trac.mystic.cacr.caltech.edu/project/pathos
 
 or::
@@ -64,7 +64,7 @@ or::
 or also::
     https://pypi.python.org/pypi/pox
 
-Pox is distributed under a 3-clause BSD license.
+`pox` is distributed under a 3-clause BSD license.
 
 
 Development Version
@@ -72,32 +72,31 @@ Development Version
 You can get the latest development version with all the shiny new features at::
     https://github.com/uqfoundation
 
-Feel free to fork the github mirror of our svn trunk.  If you have a new
-contribution, please submit a pull request.
+If you have a new contribution, please submit a pull request.
 
 
 More Information
 ----------------
 Probably the best way to get started is to look at the tests that are
-provided within pox. See `pox.tests` for a set of scripts that demonstrate
-pox's ability to interact with the operating system.  Pox utilities can
+provided within `pox`. See `pox.tests` for a set of scripts that demonstrate
+how `pox` can be used to interact with the operating system. `pox` utilities
 also be run directly from an operating system terminal, using the
 `pox_launcher.py` script.  The source code is also generally well
 documented, so further questions may be resolved by inspecting the code
 itself.  Please also feel free to submit a ticket on github, or ask a
 question on stackoverflow (@Mike McKerns).
 
-Pox is an active research tool. There are a growing number of publications
-and presentations that discuss real-world examples and new features of pox
+`pox` is an active research tool. There are a growing number of publications
+and presentations that discuss real-world examples and new features of `pox`
 in greater detail than presented in the user's guide.  If you would like to
-share how you use pox in your work, please post a link or send an email
-(to mmckerns at caltech dot edu).
+share how you use `pox` in your work, please post a link or send an email
+(to mmckerns at uqfoundation dot org).
 
 
 Citation
 --------
-If you use pox to do research that leads to publication, we ask that you
-acknowledge use of pox by citing the following in your publication::
+If you use `pox` to do research that leads to publication, we ask that you
+acknowledge use of `pox` by citing the following in your publication::
 
     M.M. McKerns, L. Strand, T. Sullivan, A. Fang, M.A.G. Aivazis,
     "Building a framework for predictive science", Proceedings of
--- setup.py
+++ setup.py
@@ -54,17 +54,17 @@ pox: utilities for filesystem exploration and automated builds
 About Pox
 =========
 
-Pox provides a collection of utilities for navigating and manipulating
+`pox` provides a collection of utilities for navigating and manipulating
 filesystems. This module is designed to facilitate some of the low level
 operating system interactions that are useful when exploring a filesystem
 on a remote host, where queries such as "what is the root of the filesystem?",
 "what is the user's name?", and "what login shell is preferred?" become
 essential in allowing a remote user to function as if they were logged in
-locally. While pox is in the same vein of both the `os` and `shutil`
+locally. While `pox` is in the same vein of both the `os` and `shutil`
 builtin modules, the majority of its functionality is unique and compliments
 these two modules.
 
-Pox provides python equivalents of several unix shell commands such as
+`pox` provides python equivalents of several unix shell commands such as
 "which" and "find". These commands allow automated discovery of what has
 been installed on an operating system, and where the essential tools are
 located. This capability is useful not only for exploring remote hosts,
@@ -75,13 +75,13 @@ Examples of which are: finding the location of an installed python package,
 determining if and where the source code resides on the filesystem, and
 determining what version the installed package is.
 
-Pox also provides utilities to enable the abstraction of commands sent
+`pox` also provides utilities to enable the abstraction of commands sent
 to a remote filesystem.  In conjunction with a registry of environment
-variables and installed utilites, pox enables the user to interact with
+variables and installed utilites, `pox` enables the user to interact with
 a remote filesystem as if they were logged in locally. 
 
-Pox is part of pathos, a python framework for heterogeneous computing.
-Pox is in active development, so any user feedback, bug reports, comments,
+`pox` is part of `pathos`, a python framework for heterogeneous computing.
+`pox` is in active development, so any user feedback, bug reports, comments,
 or suggestions are highly appreciated.  A list of known issues is maintained
 at http://trac.mystic.cacr.caltech.edu/project/pathos/query, with a public
 ticket list at https://github.com/uqfoundation/pox/issues.
@@ -90,13 +90,13 @@ ticket list at https://github.com/uqfoundation/pox/issues.
 Major Features
 ==============
 
-Pox provides utilities for discovering the user's environment::
+`pox` provides utilities for discovering the user's environment::
 
     - return the user's name, current shell, and path to user's home directory
     - strip duplicate entries from the user's $PATH
     - lookup and expand environment variables from ${VAR} to 'value'
 
-Pox also provides utilities for filesystem exploration and manipulation::
+`pox` also provides utilities for filesystem exploration and manipulation::
 
     - discover the path to a file, exectuable, directory, or symbolic link 
     - discover the path to an installed package
@@ -107,9 +107,9 @@ Pox also provides utilities for filesystem exploration and manipulation::
 Current Release
 ===============
 
-This version is pox-%(relver)s.
+This version is `pox-%(relver)s`.
 
-The latest stable release of pox is available from::
+The latest released version of `pox` is available from::
 
     http://trac.mystic.cacr.caltech.edu/project/pathos
 
@@ -121,7 +121,7 @@ or also::
 
     https://pypi.python.org/pypi/pox
 
-Pox is distributed under a 3-clause BSD license.
+`pox` is distributed under a 3-clause BSD license.
 
     >>> import pox
     >>> print (pox.license())
@@ -134,14 +134,13 @@ You can get the latest development version with all the shiny new features at::
 
     https://github.com/uqfoundation
 
-Feel free to fork the github mirror of our svn trunk.  If you have a new
-contribution, please submit a pull request.
+If you have a new contribution, please submit a pull request.
 
 
 Installation
 ============
 
-Pox is packaged to install from source, so you must
+`pox` is packaged to install from source, so you must
 download the tarball, unzip, and run the installer::
 
     [download]
@@ -153,7 +152,7 @@ download the tarball, unzip, and run the installer::
 You will be warned of any missing dependencies and/or settings
 after you run the "build" step above. 
 
-Alternately, pox can be installed with pip or easy_install::
+Alternately, `pox` can be installed with `pip` or `easy_install`::
 
     $ pip install pox
 
@@ -161,7 +160,7 @@ Alternately, pox can be installed with pip or easy_install::
 Requirements
 ============
 
-Pox requires::
+`pox` requires::
 
     - python2, version >= 2.5  *or*  python3, version >= 3.1
 
@@ -174,26 +173,26 @@ More Information
 ================
 
 Probably the best way to get started is to look at the tests that are
-provided within pox. See `pox.tests` for a set of scripts that demonstrate
-pox's ability to interact with the operating system.  Pox utilities can
-also be run directly from an operating system terminal, using the
+provided within `pox`. See `pox.tests` for a set of scripts that demonstrate
+how `pox` can be used to interact with the operating system. `pox` utilities
+can also be run directly from an operating system terminal, using the
 `pox_launcher.py` script.  The source code is also generally well
 documented, so further questions may be resolved by inspecting the code
 itself.  Please also feel free to submit a ticket on github, or ask a
 question on stackoverflow (@Mike McKerns).
 
-Pox is an active research tool. There are a growing number of publications
-and presentations that discuss real-world examples and new features of pox
+`pox` is an active research tool. There are a growing number of publications
+and presentations that discuss real-world examples and new features of `pox`
 in greater detail than presented in the user's guide.  If you would like to
-share how you use pox in your work, please post a link or send an email
-(to mmckerns at caltech dot edu).
+share how you use `pox` in your work, please post a link or send an email
+(to mmckerns at uqfoundation dot org).
 
 
 Citation
 ========
 
-If you use pox to do research that leads to publication, we ask that you
-acknowledge use of pox by citing the following in your publication::
+If you use `pox` to do research that leads to publication, we ask that you
+acknowledge use of `pox` by citing the following in your publication::
 
     M.M. McKerns, L. Strand, T. Sullivan, A. Fang, M.A.G. Aivazis,
     "Building a framework for predictive science", Proceedings of
@@ -239,7 +238,7 @@ setup(name='pox',
       long_description = '''%s''',
       author = 'Mike McKerns',
       maintainer = 'Mike McKerns',
-      maintainer_email = 'mmckerns@caltech.edu',
+      maintainer_email = 'mmckerns@uqfoundation.org',
       license = 'BSD',
       platforms = ['any'],
       url = 'http://www.cacr.caltech.edu/~mmckerns',
