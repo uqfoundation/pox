776a533a3304dc6a829deb31e612d0405cf64517
mmckerns <mmckerns@caltech.edu>
2020-11-01 18:33:24 -0500

    tag 0.2.9

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ if unsupported:
     raise ValueError(unsupported)
 
 # set version numbers
-stable_version = '0.2.8'
+stable_version = '0.2.9'
 target_version = '0.2.9'
 is_release = stable_version == target_version
 
