3ab256fe0ad7500d83c1374fdc5007bb33550579
mmckerns <mmckerns@caltech.edu>
2022-05-06 20:07:33 -0400

    build changes to support pip and pep517

--- setup.py
+++ setup.py
@@ -17,264 +17,90 @@ elif (3, 0) <= sys.version_info < (3, 7):
 if unsupported:
     raise ValueError(unsupported)
 
-# set version numbers
-stable_version = '0.3.0'
-target_version = '0.3.1'
-is_release = stable_version == target_version
-
-# check if easy_install is available
 try:
-#   import __force_distutils__ #XXX: uncomment to force use of distutills
+    import builtins
+except ImportError:
+    import __builtin__ as builtins
+
+# This is a hack to import a minimal package for the build process
+builtins.__POX_SETUP__ = True
+sys.path.append(os.path.abspath(os.path.curdir))
+import pox
+
+# get version numbers, long_description, etc
+AUTHOR = pox.__author__
+VERSION = pox.__version__
+LONG_DOC = pox.__doc__ #FIXME: near-duplicate of README.md
+#LICENSE = pox.__license__ #FIXME: duplicate of LICENSE
+AUTHOR_EMAIL = 'mmckerns@uqfoundation.org'
+
+# check if setuptools is available
+try:
     from setuptools import setup
+    from setuptools.dist import Distribution
     has_setuptools = True
 except ImportError:
     from distutils.core import setup
+    Distribution = object
     has_setuptools = False
 
-# generate version number
-if os.path.exists('pox/info.py'):
-    # is a source distribution, so use existing version
-    os.chdir('pox')
-    with open('info.py','r') as f:
-        f.readline() # header
-        this_version = f.readline().split()[-1].strip("'")
-    os.chdir('..')
-elif stable_version == target_version:
-    # we are building a stable release
-    this_version = target_version
-else:
-    # we are building a distribution
-    this_version = target_version + '.dev0'
-    if is_release:
-      from datetime import date
-      today = "".join(date.isoformat(date.today()).split('-'))
-      this_version += "-" + today
-
-# get the license info
-with open('LICENSE') as file:
-    license_text = file.read()
-
-# generate the readme text
-long_description = \
-"""--------------------------------------------------------------
-pox: utilities for filesystem exploration and automated builds
---------------------------------------------------------------
-
-About Pox
-=========
-
-``pox`` provides a collection of utilities for navigating and manipulating
-filesystems. This module is designed to facilitate some of the low level
-operating system interactions that are useful when exploring a filesystem
-on a remote host, where queries such as *"what is the root of the filesystem?"*,
-*"what is the user's name?"*, and *"what login shell is preferred?"* become
-essential in allowing a remote user to function as if they were logged in
-locally. While ``pox`` is in the same vein of both the ``os`` and ``shutil``
-builtin modules, the majority of its functionality is unique and compliments
-these two modules.
-
-``pox`` provides python equivalents of several unix shell commands such as
-``which`` and ``find``. These commands allow automated discovery of what has
-been installed on an operating system, and where the essential tools are
-located. This capability is useful not only for exploring remote hosts,
-but also locally as a helper utility for automated build and installation.
-
-Several high-level operations on files and filesystems are also provided.
-Examples of which are: finding the location of an installed python package,
-determining if and where the source code resides on the filesystem, and
-determining what version the installed package is.
-
-``pox`` also provides utilities to enable the abstraction of commands sent
-to a remote filesystem.  In conjunction with a registry of environment
-variables and installed utilites, ``pox`` enables the user to interact with
-a remote filesystem as if they were logged in locally. 
-
-``pox`` is part of ``pathos``, a python framework for heterogeneous computing.
-``pox`` is in active development, so any user feedback, bug reports, comments,
-or suggestions are highly appreciated.  A list of issues is located at https://github.com/uqfoundation/pox/issues, with a legacy list maintained at https://uqfoundation.github.io/project/pathos/query.
-
-
-Major Features
-==============
-
-``pox`` provides utilities for discovering the user's environment:
-
-    - return the user's name, current shell, and path to user's home directory
-    - strip duplicate entries from the user's ``$PATH``
-    - lookup and expand environment variables from ``${VAR}`` to ``value``
-
-``pox`` also provides utilities for filesystem exploration and manipulation:
-
-    - discover the path to a file, exectuable, directory, or symbolic link 
-    - discover the path to an installed package
-    - parse operating system commands for remote shell invocation
-    - convert text files to platform-specific formatting
-
-
-Current Release
-===============
-
-This documentation is for version ``pox-%(thisver)s``.
-
-The latest released version of ``pox`` is available from:
-
-    https://pypi.org/project/pox
-
-``pox`` is distributed under a 3-clause BSD license.
-
-    >>> import pox
-    >>> pox.license()
-
-
-Development Version
-===================
-
-You can get the latest development version with all the shiny new features at:
-
-    https://github.com/uqfoundation
-
-If you have a new contribution, please submit a pull request.
-
-
-Installation
-============
-
-``pox`` is packaged to install from source, so you can
-download the tarball, unzip, and run the installer::
-
-    [download]
-    $ tar -xvzf pox-%(relver)s.tar.gz
-    $ cd pox-%(relver)s
-    $ python setup py build
-    $ python setup py install
-
-You will be warned of any missing dependencies and/or settings
-after you run the "build" step above. 
-
-Alternately, ``pox`` can be installed with ``pip`` or ``easy_install``::
-
-    $ pip install pox
-
-
-Requirements
-============
-
-``pox`` requires:
-
-    - ``python`` (or ``pypy``), **version == 2.7** or **version >= 3.7**
-
-Optional requirements:
-
-    - ``setuptools``, **version >= 40.6.0**
-
-
-More Information
-================
-
-Probably the best way to get started is to look at the documentation at
-http://pox.rtfd.io. Also see ``pox.tests`` for a set of scripts that demonstrate
-how ``pox`` can be used to interact with the operating system. You can run the
-test suite with ``python -m pox.tests``.  All ``pox`` utilities
-can be launched directly from an operating system terminal, using the ``pox``
-script (or with ``python -m pox``).  The source code is also generally well
-documented, so further questions may be resolved by inspecting the code
-itself.  Please feel free to submit a ticket on github, or ask a
-question on stackoverflow (**@Mike McKerns**).
-If you would like to share how you use ``pox`` in your work, please send an
-email (to **mmckerns at uqfoundation dot org**).
-
-
-Citation
-========
-
-If you use ``pox`` to do research that leads to publication, we ask that you
-acknowledge use of ``pox`` by citing the following in your publication::
-
-    M.M. McKerns, L. Strand, T. Sullivan, A. Fang, M.A.G. Aivazis,
-    "Building a framework for predictive science", Proceedings of
-    the 10th Python in Science Conference, 2011;
-    http://arxiv.org/pdf/1202.1056
-
-    Michael McKerns and Michael Aivazis,
-    "pathos: a framework for heterogeneous computing", 2010- ;
-    https://uqfoundation.github.io/project/pathos
-
-Please see https://uqfoundation.github.io/project/pathos or
-http://arxiv.org/pdf/1202.1056 for further information.
-
-""" % {'relver' : stable_version, 'thisver' : this_version}
-
-# write readme file
-with open('README', 'w') as file:
-    file.write(long_description)
-
-# generate 'info' file contents
-def write_info_py(filename='pox/info.py'):
-    contents = """# THIS FILE GENERATED FROM SETUP.PY
-this_version = '%(this_version)s'
-stable_version = '%(stable_version)s'
-readme = '''%(long_description)s'''
-license = '''%(license_text)s'''
-"""
-    with open(filename, 'w') as file:
-        file.write(contents % {'this_version' : this_version,
-                               'stable_version' : stable_version,
-                               'long_description' : long_description,
-                               'license_text' : license_text })
-    return
-
-# write info file
-write_info_py()
-
 # build the 'setup' call
-setup_code = """
-setup(name='pox',
-      version='%s',
-      description='utilities for filesystem exploration and automated builds',
-      long_description = '''%s''',
-      author = 'Mike McKerns',
-      maintainer = 'Mike McKerns',
-      license = '3-clause BSD',
-      platforms = ['Linux', 'Windows', 'Mac'],
-      url = 'https://github.com/uqfoundation/pox',
-      download_url = 'https://github.com/uqfoundation/pox/releases/download/pox-%s/pox-%s.tar.gz',
-      python_requires = '>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, !=3.6.*',
-      classifiers = ['Development Status :: 5 - Production/Stable',
-                     'Intended Audience :: Developers',
-                     'Intended Audience :: Science/Research',
-                     'License :: OSI Approved :: BSD License',
-                     'Programming Language :: Python :: 2',
-                     'Programming Language :: Python :: 2.7',
-                     'Programming Language :: Python :: 3',
-                     'Programming Language :: Python :: 3.7',
-                     'Programming Language :: Python :: 3.8',
-                     'Programming Language :: Python :: 3.9',
-                     'Programming Language :: Python :: 3.10',
-                     'Programming Language :: Python :: Implementation :: PyPy',
-                     'Topic :: Scientific/Engineering',
-                     'Topic :: Software Development'],
+setup_kwds = dict(
+    name='pox',
+    version=VERSION,
+    description='utilities for filesystem exploration and automated builds',
+    long_description = LONG_DOC,
+    author = AUTHOR,
+    author_email = AUTHOR_EMAIL,
+    maintainer = AUTHOR,
+    maintainer_email = AUTHOR_EMAIL,
+    license = '3-clause BSD',
+    platforms = ['Linux', 'Windows', 'Mac'],
+    url = 'https://github.com/uqfoundation/pox',
+    download_url = 'https://pypi.org/project/pox/#files',
+    python_requires = '>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, !=3.6.*',
+    classifiers = ['Development Status :: 5 - Production/Stable',
+                   'Intended Audience :: Developers',
+                   'Intended Audience :: Science/Research',
+                   'License :: OSI Approved :: BSD License',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.7',
+                   'Programming Language :: Python :: 3.8',
+                   'Programming Language :: Python :: 3.9',
+                   'Programming Language :: Python :: 3.10',
+                   'Programming Language :: Python :: Implementation :: PyPy',
+                   'Topic :: Scientific/Engineering',
+                   'Topic :: Software Development'],
+    packages = ['pox','pox.tests'],
+    package_dir = {'pox':'pox','pox.tests':'tests'},
+    scripts=['scripts/pox'],
+)
 
-      packages = ['pox','pox.tests'],
-      package_dir = {'pox':'pox','pox.tests':'tests'},
-""" % (target_version, long_description, stable_version, stable_version)
+# force python-, abi-, and platform-specific naming of bdist_wheel
+class BinaryDistribution(Distribution):
+    """Distribution which forces a binary package with platform name"""
+    def has_ext_modules(foo):
+        return True
 
-'''
+# define dependencies
+dummy_version = 'dummy>=0.1'
+extra_version = 'extra>=0.1'
 # add dependencies
-dummy_version = '>=0.1'
+depend = [dummy_version]
+extras = {'extra': [extra_version]}
+# update setup kwds
 if has_setuptools:
-    setup_code += """
-        install_requires = ['dummy%s'],
-""" % (dummy_version)
-'''
+    setup_kwds.update(
+        zip_safe=False,
+        # distclass=BinaryDistribution,
+        # install_requires=depend,
+        # extras_require=extras,
+    )
 
-# close 'setup' call
-setup_code += """    
-      zip_safe=False,
-      scripts=['scripts/pox'])
-"""
-
-# exec the 'setup' code
-exec(setup_code)
+# call setup
+setup(**setup_kwds)
 
 # if dependencies are missing, print a warning
 try:
@@ -282,7 +108,7 @@ try:
 except ImportError:
     print("\n***********************************************************")
     print("WARNING: One of the following dependencies is unresolved:")
-#   print("    dummy %s" % dummy_version)
+#   print("    %s" % dummy_version)
     print("***********************************************************\n")
 
 
