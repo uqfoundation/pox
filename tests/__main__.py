b5404cd3e6bda3f73d7829d0ddc04bc780bc89f9
mmckerns <mmckerns@caltech.edu>
2022-06-26 09:28:26 -0400

    better python handling in test main

--- tests/__main__.py
+++ tests/__main__.py
@@ -5,17 +5,16 @@
 # License: 3-clause BSD.  The full license text is available at:
 #  - https://github.com/uqfoundation/pox/blob/master/LICENSE
 
-from __future__ import print_function
 import glob
 import os
-import pox
+import sys
 import subprocess as sp
-from sys import platform
-shell = platform[:3] == 'win'
+import pox
+shell = sys.platform[:3] == 'win'
 
 suite = os.path.dirname(__file__) or os.path.curdir
 tests = glob.glob(suite + os.path.sep + 'test_*.py')
-python = pox.which_python(version=True, fullpath=False) or 'python'
+python = pox.which_python(version=True) or sys.executable
 
 if __name__ == '__main__':
 
@@ -24,6 +23,3 @@ if __name__ == '__main__':
         if not p:
             print('.', end='', flush=True)
     print('')
-
-
-#XXX: unittest.main() ?
